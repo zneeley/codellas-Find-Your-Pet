6c77b4b548d793ea42fc7f1e839986bcf2ca9ec0
Joshua Alexander <loljos6@gmail.com>
2019-09-30 22:55:15 -0600

    Improved the index page
    -Also installed bootstrap 4

--- loginSystem/index.php
+++ loginSystem/index.php
@@ -4,17 +4,28 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+<style>
+</style>
 <html>
     <head>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <meta charset="UTF-8">
         <title>Codellas Web Login System</title>
     </head>
     <body>
-        <?php
-        
-        ?>
-        <h2>Stuff should go on this page!!!</h2>
-        <a href="register.php">Register</a><br>
-        <a href="login.php">Login</a>
+        <div class="container" style="width"18rem">
+			<div class="card text-center w-50 border border-info" style="margin: 0 auto 0 auto; margin-top: 10rem">
+				<div class="card-header">
+					<h1 class="card-title">Find Your Pet!</h1>
+				</div>
+				<div class="card-body">
+					<a href="register.php">Register</a><br>
+					<a href="login.php">Login</a>
+				</div>
+			</div>
+			<footer class="page-footer font-small text-center">
+					This will be a footer soon!
+			</footer>
+		</div>
     </body>
 </html>
