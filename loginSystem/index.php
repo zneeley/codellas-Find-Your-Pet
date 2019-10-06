4a7d62b219dd61816094daa0483c54fd4dea2680
Sikta <sikta@nmsu.edu>
2019-10-06 14:57:17 -0600

    Added css and background images

--- loginSystem/index.php
+++ loginSystem/index.php
@@ -1,31 +1,50 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<style>
-</style>
 <html>
-    <head>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <meta charset="UTF-8">
-        <title>Codellas Web Login System</title>
-    </head>
-    <body>
-        <div class="container" style="width"18rem">
-			<div class="card text-center w-50 border border-info" style="margin: 0 auto 0 auto; margin-top: 10rem">
-				<div class="card-header">
-					<h1 class="card-title">Find Your Pet!</h1>
-				</div>
-				<div class="card-body">
-					<a href="register.php">Register</a><br>
-					<a href="login.php">Login</a>
-				</div>
-			</div>
-			<footer class="page-footer font-small text-center">
-					This will be a footer soon!
-			</footer>
-		</div>
+<head>
+<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+       
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+         
+        <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <body class="text-center">
+
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column hero-image">
+  <header class="masthead mb-auto">
+    <div class="inner">
+        <div class="logo-image"><img src="/images/image.png" alt="logo" width=auto height="250"></div>
+<!--
+      <nav class="nav nav-masthead justify-content-center">
+        <a class="nav-link active" href="#">Home</a>
+        <a class="nav-link" href="#">Contact</a> 
+      </nav> -->
+    </div>
+  </header>
+
+  <main role="main" class="inner cover">
+    <h1 class="cover-heading">Find your Pet</h1>
+    <p class="lead">We make adopting pets and finding your forever best friend easier!</p>
+    <p class="lead">
+      <a href="register.php" class="btn btn-lg btn-secondary">Register</a>
+      <a href="login.php" class="btn btn-lg btn-secondary">Login</a>
+    </p>
+  </main>
+
+  <footer class="mastfoot mt-auto">
+    <div class="inner">
+      <p>@2019 Find your Pet</p>
+    </div>
+  </footer>
+</div>
+     
+   
+        
+   </body>
     </body>
+    
 </html>
+
