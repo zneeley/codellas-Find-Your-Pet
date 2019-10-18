ccbf7c038d5676a9fbeb577fad3dcc2651900774
Seth Hager <shager@nmsu.edu>
2019-10-17 18:42:08 -0600

    condensed database files
    condensed users.sql and shelters.sql into websiteDatabase.sql

--- sql Database Import Info/websiteDatabase.sql
+++ sql Database Import Info/websiteDatabase.sql
@@ -68,6 +68,46 @@ ALTER TABLE `shelters`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `userID` varchar(100) NOT NULL,
+  `FirstName` varchar(30) NOT NULL,
+  `LastName` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `userID`, `FirstName`, `LastName`, `email`, `username`, `password`, `created_at`) VALUES
+(17, 'UUID-5d9fb6de8ea6d', 'Zach', 'Neeley', 'em5lZWxleUBubXN1LmVkdQ==', 'zneeley', '$2y$10$R4JSBI4qaXJ2iq/YOR00LeO0jJFE98c4qbwtwMmU9VfUS6E2fznJC', '2019-10-10 16:55:26');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
